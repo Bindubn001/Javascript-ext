fb0d65b992ecee0b4f7be918b1a3874268c3f3fd
Bindubn001 <bnbindu879@gmail.com>
2023-11-12 11:15:45 +0530

    'modified'

--- popup.js
+++ popup.js
@@ -1,49 +1,33 @@
 console.log("hello bindiu");
 let jsonData = {};
 
-// fetch('https://654cc48877200d6ba8596285.mockapi.io/urls')
-//   .then(response => {
-//     // Check if the response is successful (status code 200-299)
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     // Parse the response as JSON
-//     return response.json();
-//   })
-//   .then(data => {
-//     // Store the JSON data in a variable
-//     jsonData = data;
-//     console.log('JSON Data:', jsonData[0].url);
-
-//     // Now that jsonData is populated, proceed with further operations
-//     const urls = jsonData.map(item => item.url);
-
-//     // Display the URLs in the popup
-//     const ulElement = document.createElement("ul");
-//     urls.forEach(url => {
-//       const liElement = document.createElement("li");
-//       liElement.textContent = url;
-//       ulElement.appendChild(liElement);
-//     });
-
-//     document.body.appendChild(ulElement);
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
-
-// Node.js example
-const axios = require('axios');
-
-// Browser example (if included from CDN)
-// const axios = window.axios;
-
-// Make a GET request
-axios.get('https://jsonplaceholder.typicode.com/posts/1')
+fetch('https://654cc48877200d6ba8596285.mockapi.io/urls')
   .then(response => {
-    console.log('Response:', response.data);
+   
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+   
+    return response.json();
+  })
+  .then(data => {
+ 
+    jsonData = data;
+    console.log('JSON Data:', jsonData[0].url);
+
+  
+    const urls = jsonData.map(item => item.url);
+
+   
+    const ulElement = document.createElement("ul");
+    urls.forEach(url => {
+      const liElement = document.createElement("li");
+      liElement.textContent = url;
+      ulElement.appendChild(liElement);
+    });
+
+    document.body.appendChild(ulElement);
   })
   .catch(error => {
     console.error('Error:', error);
   });
-
